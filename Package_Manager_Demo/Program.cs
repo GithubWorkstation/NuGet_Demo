a25a88c74d028556b6e4136ed15c4515dc621056
unknown <githubworkstation@gmail.com>
2024-07-12 05:37:43 +0000

    first commit

---
+++ Package_Manager_Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Package_Manager_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JArray array = new JArray();
+            JValue text = new JValue("Manual text");
+            JValue date = new JValue(new DateTime(2000, 5, 23));
+
+            array.Add(text);
+            array.Add(date);
+
+            string json = array.ToString();
+            Console.WriteLine(json);
+        }
+    }
+}
